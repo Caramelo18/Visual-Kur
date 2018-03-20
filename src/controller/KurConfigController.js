2133186a0c7ab767e5a8a954ef3b8831d5e40d07
Caramelo18 <fabiocaramelo18@gmail.com>
2018-03-20 10:50:33 +0000

    started parser

--- src/controller/KurConfigController.js
+++ src/controller/KurConfigController.js
@@ -1,6 +1,28 @@
 import KurConfigModel from '../model/KurConfigModel'
+import yamljs from 'yamljs';
 
 export default class KurConfigController {
+  constructor() {
+    this.loadFile('./mnist.yml');
+  }
+
+  loadFile(filename){
+    const file = yamljs.load(filename);
+    this.parseFile(file.model);
+  }
+
+  parseFile(file){
+    this.model = new KurConfigModel();
+
+    console.log(Object.keys(file));
+    for(let i = 0; i < file.length; i++){
+      let element = file[i];
+      console.log(element);
+    }
+
+
+  }
+
   getExample () {
     let exampleModel = new KurConfigModel();
     return exampleModel.example;
--- src/model/KurConfigModel.js
+++ src/model/KurConfigModel.js
@@ -1,3 +1,18 @@
 export default class KurConfigModel {
+  input = "";
+  layers = [];
+  activation = []
   example = "This is working.";
+
+  setInput(input){
+    this.input = input;
+  }
+
+  addLayer(layer){
+    this.layers.push(layer);
+  }
+
+  setActivation(activation){
+    this.activation = activation;
+  }
 }
