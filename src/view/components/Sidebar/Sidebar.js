cf8afafe5c86795fbdca812d6bc96dbea40ef501
Diogo Campos <d.diogocampos@gmail.com>
2018-03-20 16:05:36 +0000

    Add AppBar and FilePicker components to Sidebar

--- src/view/components/Sidebar/Sidebar.js
+++ src/view/components/Sidebar/Sidebar.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import FilePicker from './FilePicker';
 
-export default class Sidebar extends React.Component {
+const drawerWidth = 350;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'absolute',
+    width: drawerWidth,
+  },
+});
+
+class Sidebar extends React.Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <Drawer>
-        <AppBar title="Visual Kur" showMenuIconButton={false}/>
-      </Drawer>
+      <div className={classes.root}>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Visual Kur
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <FilePicker/>
+        </Drawer>
+      </div>
     )
   }
 }
+
+Sidebar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Sidebar);
