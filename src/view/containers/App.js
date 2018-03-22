784ae6d565d199f9afd3fc140ae255d51fd3b846
Diogo Campos <d.diogocampos@gmail.com>
2018-03-22 13:05:40 +0000

    Sidebar File Picker (#1)
    * Add material-ui
    
    * Create Sidebar with Drawer
    
    * Using material-ui v1.0
    
    * Create FilePicker button
    
    * Add AppBar and FilePicker components to Sidebar
    
    * Use Sidebar in App
    
    * Picking file and storing in FilePicker state
    
    * Styling AppBar and FilePicker
    
    * Clean code

--- src/view/containers/App.js
+++ src/view/containers/App.js
@@ -1,7 +1,26 @@
-import KurConfigController from '../../controller/KurConfigController';
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import Sidebar from '../components/Sidebar/Sidebar';
+import KurConfigController from '../../controller/KurConfigController';
 import logo from '../images/logo.svg';
 import '../styles/App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 class App extends Component {
   componentWillMount() {
@@ -10,19 +29,22 @@ class App extends Component {
     this.setState({
       exampleController,
     });
-  }
+  };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.exampleController.getExample()}
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Sidebar/>
+          <p className="App-intro">
+            {this.state.exampleController.getExample()}
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
